84304cd848eb9b90e1373978dc40432368c29213
Imron <imron.gamidli@lt.hs-fulda.de>
2022-06-27 21:28:41 +0200

    feat: retrieve item details in ItemDetailFragment

--- app/src/main/java/com/example/inventory/InventoryViewModel.kt
+++ app/src/main/java/com/example/inventory/InventoryViewModel.kt
@@ -10,33 +10,37 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 
 class InventoryViewModel(private val itemDao: ItemDao) : ViewModel() {
-  val allItems: LiveData<List<Item>> = itemDao.getItems().asLiveData()
+    val allItems: LiveData<List<Item>> = itemDao.getItems().asLiveData()
+    lateinit var item: Item
 
-  private fun insertItem(item: Item) {
-    viewModelScope.launch {
-      itemDao.insert(item)
+    private fun insertItem(item: Item) {
+        viewModelScope.launch {
+            itemDao.insert(item)
+        }
     }
-  }
 
-  private fun getNewItemEntry(itemName: String, itemPrice: String, itemCount: String): Item {
-    return Item(
-      itemName = itemName,
-      itemPrice = itemPrice.toDouble(),
-      quantityInStock = itemCount.toInt()
-    )
-  }
-
-  fun addNewItem(itemName: String, itemPrice: String, itemCount: String) {
-    val newItem = getNewItemEntry(itemName, itemPrice, itemCount)
-    insertItem(newItem)
-  }
-
-  fun isEntryValid(itemName: String, itemPrice: String, itemCount: String): Boolean {
-    if (itemName.isBlank() || itemPrice.isBlank() || itemCount.isBlank()) {
-      return false
+    private fun getNewItemEntry(itemName: String, itemPrice: String, itemCount: String): Item {
+        return Item(
+                itemName = itemName,
+                itemPrice = itemPrice.toDouble(),
+                quantityInStock = itemCount.toInt()
+        )
     }
-    return true
-  }
 
-  fun getItems(): Flow<List<Item>> = itemDao.getItems()
+    fun addNewItem(itemName: String, itemPrice: String, itemCount: String) {
+        val newItem = getNewItemEntry(itemName, itemPrice, itemCount)
+        insertItem(newItem)
+    }
+
+    fun isEntryValid(itemName: String, itemPrice: String, itemCount: String): Boolean {
+        if (itemName.isBlank() || itemPrice.isBlank() || itemCount.isBlank()) {
+            return false
+        }
+        return true
+    }
+
+    fun getItems(): Flow<List<Item>> = itemDao.getItems()
+
+    fun retrieveItem(id: Int): LiveData<Item> = itemDao.getItem(id).asLiveData()
+
 }
--- app/src/main/java/com/example/inventory/ItemDetailFragment.kt
+++ app/src/main/java/com/example/inventory/ItemDetailFragment.kt
@@ -22,8 +22,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.example.inventory.data.Item
+import com.example.inventory.data.getFormattedPrice
 import com.example.inventory.databinding.FragmentItemDetailBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
@@ -36,13 +39,25 @@ class ItemDetailFragment : Fragment() {
     private var _binding: FragmentItemDetailBinding? = null
     private val binding get() = _binding!!
 
+    private val viewModel: InventoryViewModel by activityViewModels {
+        InventoryViewModelFactory(
+                (activity?.application as InventoryApplication).database.itemDao()
+        )
+    }
+
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View {
         _binding = FragmentItemDetailBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val id = navigationArgs.itemId
+        viewModel.retrieveItem(id).observe(this.viewLifecycleOwner) { bind(it) }
     }
 
     /**
@@ -50,14 +65,14 @@ class ItemDetailFragment : Fragment() {
      */
     private fun showConfirmationDialog() {
         MaterialAlertDialogBuilder(requireContext())
-            .setTitle(getString(android.R.string.dialog_alert_title))
-            .setMessage(getString(R.string.delete_question))
-            .setCancelable(false)
-            .setNegativeButton(getString(R.string.no)) { _, _ -> }
-            .setPositiveButton(getString(R.string.yes)) { _, _ ->
-                deleteItem()
-            }
-            .show()
+                .setTitle(getString(android.R.string.dialog_alert_title))
+                .setMessage(getString(R.string.delete_question))
+                .setCancelable(false)
+                .setNegativeButton(getString(R.string.no)) { _, _ -> }
+                .setPositiveButton(getString(R.string.yes)) { _, _ ->
+                    deleteItem()
+                }
+                .show()
     }
 
     /**
@@ -73,5 +88,13 @@ class ItemDetailFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun bind(item: Item) {
+        binding.apply {
+            itemName.text = item.itemName
+            itemPrice.text = item.getFormattedPrice()
+            itemCount.text = item.quantityInStock.toString()
+        }
     }
 }
